88b6e60688e202e63df9bc0335ea4368f3b25225
Think Code Play <johnwebdev20@gmail.com>
2022-08-23 21:55:47 +0300

    vercel deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  boundaries_url:
+  'https://vanitysoft-boundaries-io-v1.p.rapidapi.com/rest/v1/public',
+'X-RapidAPI-Key': 'f14dfc7e38msh6c462596c42d6aep12ac80jsn9d9cb5cf818c',
+'X-RapidAPI-Host': 'vanitysoft-boundaries-io-v1.p.rapidapi.com',
+mapbox: {
+  accessToken:
+    'pk.eyJ1IjoiandlYmRldiIsImEiOiJjbDc0ZmU1NjAwOW90M250aDJtZG5tMmJiIn0.YdqgvikWgQKr1Yvq45N75Q',
+},
+weather_key: "692e24bb928b4bb5bad200827222208"
 };
